b7b5b63e8b0083fe48debcefeee2f12e79a5bdf2
DesarrolloIT-A3com <desarrolloit@a3com.es>
2026-07-23 07:19:36 +0200

    Se actualiza la visualización del proyecto añadiendo los pluses dentro del precio

--- include/secciones/proyectos/ver_proyecto.php
+++ include/secciones/proyectos/ver_proyecto.php
@@ -36,6 +36,147 @@ $interior_puerta_8 = explode("-", $proyecto['interior_puerta_8']);
 $serie = $db->getVar('SELECT nombre FROM series WHERE id=' . $proyecto['id_serie']);
 $acabado = $db->getVar('SELECT nombre FROM acabados WHERE id=' . $proyecto['id_acabado']);
 $perfileria = $db->getRow('SELECT nombre, imagen FROM colores WHERE id=' . $proyecto['id_color_perfileria']);
+$tarifa = $db->getVar('SELECT porcentaje FROM tarifa WHERE id='.$proyecto['id_tarifa']);
+
+function extractColorInterior($num_modulos,$db)
+{
+	$plus_cream_stone = 0;
+	$plus_grey_stone = 0;
+
+	$num_modulo = 0;
+	for ($j = 1; $j <= $num_modulos; $j++) 
+	{
+		$num_modulo++;
+		global ${'interior_puerta_'.$num_modulo};
+		$nodo_puerta = ${'interior_puerta_'.$num_modulo};
+		if(count($nodo_puerta) > 9)
+		{
+			$color_interior = $db->getVar('SELECT nombre FROM colores WHERE id='.$nodo_puerta[28]);
+			$color_cantoneras = $db->getVar('SELECT nombre FROM colores WHERE id='.$nodo_puerta[29]);
+
+			if($color_interior == "Cream Stone" || $color_cantoneras == "Cream Stone")
+			{
+				$plus_cream_stone += 20;
+			}
+
+			if($color_interior == "Grey Stone" || $color_cantoneras == "Grey Stone")
+			{
+				$plus_grey_stone += 20;
+			}
+		}
+		else
+		{
+			$color_interior = $db->getVar('SELECT nombre FROM colores WHERE id='.$nodo_puerta[28]);
+			$color_cantoneras = $db->getVar('SELECT nombre FROM colores WHERE id='.$nodo_puerta[29]);
+
+			if($color_interior == "Cream Stone" || $color_cantoneras == "Cream Stone")
+			{
+				$plus_cream_stone += 20;
+			}
+
+			if($color_interior == "Grey Stone" || $color_cantoneras == "Grey Stone")
+			{
+				$plus_grey_stone += 20;
+			}
+		}
+	}
+
+	return [
+		'cream_stone' => $plus_cream_stone,
+		'grey_stone' => $plus_grey_stone
+	];
+}
+
+$plus_cream_stone = 0;
+$plus_grey_stone = 0;
+$plus_dark_grey = 0;
+$incremento_ral = 0;
+
+for($i = 1; $i <= $proyecto['num_puertas']; $i++)
+{
+	$diseno_puerta = $db->getRow('SELECT d.nombre as diseno, t.id as terminacion FROM disenos as d, terminaciones as t WHERE d.id=' . ${"diseno_puerta_" . $i}[0] . ' AND t.id=' . ${"diseno_puerta_" . $i}[1]);
+	$zonas_puerta = $db->getResults('SELECT pz.zona as zona, pz.id_colores_tipo as id_colores_tipo, ct.nombre as tipo FROM puertas_zonas as pz, disenos_puertas as dp, colores_tipo as ct WHERE pz.id_disenos_puertas = dp.id AND pz.id_colores_tipo = ct.id AND dp.id_acabados = ' . $proyecto['id_acabado'] . ' AND dp.id_disenos = ' . ${"diseno_puerta_" . $i}[0] . ' AND dp.id_terminaciones = ' . ${"diseno_puerta_" . $i}[1] . ' AND dp.id_puertas = ' . ${"diseno_puerta_" . $i}[2] . ' ORDER BY pz.zona');
+	$found_cream_stone = false;
+	$found_grey_stone = false;
+	$count_dark_grey = 0;
+	$zona_cristal = 0;
+	foreach ($zonas_puerta as $index => $zona_puerta) 
+	{
+		// Comprobamos si hay más del mismo tipo para ponerle el número detrás o no
+		$hay_mas = false;
+		if (isset($zonas_puerta[$index + 1]['id_colores_tipo']) && $zona_puerta['id_colores_tipo'] == $zonas_puerta[$index + 1]['id_colores_tipo']) 
+		{
+			$hay_mas = true;
+		}
+
+		// SE OBTIENEN LOS DATOS CORRESPONDIENTES
+		$nombre_color_zona = "";
+		if (isset(${"colores_puerta_" . $i}[$index]) && ${"colores_puerta_" . $i}[$index] > 0) 
+		{
+			$color_zona = $db->getRow('SELECT nombre, imagen FROM colores WHERE id = ' . ${"colores_puerta_" . $i}[$index]);
+			$nombre_color_zona = $color_zona['nombre'];
+		}
+
+		if($nombre_color_zona=="Cream Stone")
+		{
+			$found_cream_stone = true;
+		}
+
+		if($nombre_color_zona=="Grey Stone")
+		{
+			$found_grey_stone = true;
+		}
+
+		if($nombre_color_zona=="Dark Grey" && ($diseno_puerta['terminacion'] == 2 || $diseno_puerta['terminacion'] == 3 || $diseno_puerta['terminacion'] == 4 || $diseno_puerta['terminacion'] == 7 || $diseno_puerta['terminacion'] == 8 || $diseno_puerta['terminacion'] == 17 || $diseno_puerta['terminacion'] == 18 || $diseno_puerta['terminacion'] == 19 || $diseno_puerta['terminacion'] == 28))
+		{
+			$count_dark_grey+=1;
+		}
+		
+		if(preg_match('/^Cristal\d*$/', $zona_puerta[2]))
+		{
+			$zona_cristal++;
+		}
+
+	}
+
+	if($found_cream_stone)
+	{
+		$plus_cream_stone += 20;
+	}
+
+	if($found_grey_stone)
+	{
+		$plus_grey_stone += 20;
+	}
+
+	if($count_dark_grey>0)
+	{
+		$porcentaje_cristal = ($count_dark_grey * 1) / $zona_cristal;
+		$porcentaje = 0;
+		if($diseno_puerta['terminacion'] == 2 || $diseno_puerta['terminacion'] == 7 || $diseno_puerta['terminacion'] == 18){ $porcentaje = (0.33 * $porcentaje_cristal); }
+		if($diseno_puerta['terminacion'] == 3 || $diseno_puerta['terminacion'] == 8 || $diseno_puerta['terminacion'] == 19 || $diseno_puerta['terminacion'] == 28){ $porcentaje = (0.50 * $porcentaje_cristal); }
+		if($diseno_puerta['terminacion'] == 4 || $diseno_puerta['terminacion'] == 17){ $porcentaje = (1 * $porcentaje_cristal); }
+
+		$puerta = intval($proyecto['ancho']) / $proyecto['num_puertas']; // Ancho de una puerta
+		$plus_dark_grey += (35 * ($puerta * intval($proyecto['alto']) * $porcentaje / 10000));
+	}
+}
+
+$plus_dark_grey = $plus_dark_grey + (($plus_dark_grey*$tarifa) / 100);
+
+if($proyecto['num_modulos_interior'] > 0)
+{
+	$modulos_dobles = $proyecto['num_puertas'] - $proyecto['num_modulos_interior'];
+	$modulos_simples = $proyecto['num_modulos_interior'] - $modulos_dobles;
+
+	$res_dobles = extractColorInterior($modulos_dobles,$db);
+	$plus_cream_stone+=$res_dobles['cream_stone'];
+	$plus_grey_stone+=$res_dobles['grey_stone'];
+
+	$res_simples = extractColorInterior($modulos_simples,$db);
+	$plus_cream_stone+=$res_simples['cream_stone'];
+	$plus_grey_stone+=$res_simples['grey_stone'];
+}
 
 ?>
 <div id="ver_proyecto" class="pantalla_completa">
@@ -620,6 +761,15 @@ $perfileria = $db->getRow('SELECT nombre, imagen FROM colores WHERE id=' . $proy
 					<?php if ($proyecto['montaje_interior_seleccionado'] > 0) { ?>
 						Precio montaje interior: <span><?php echo number_format($proyecto['precio_montaje_interior'], 2, ".", ""); ?>€</span><br />
 					<?php } ?>
+					<?php if ($plus_cream_stone>0){ ?>
+						Plus Cream Stone <span><?php echo number_format($plus_cream_stone,2,".",""); ?>€</span> <br />
+					<?php }?>
+					<?php if ($plus_grey_stone>0){ ?>
+						Plus Grey Stone <span><?php echo number_format($plus_grey_stone,2,".",""); ?>€</span> <br />
+					<?php }?>
+					<?php if ($plus_dark_grey>0){ ?>
+						Plus Dark Grey <span><?php echo number_format($plus_dark_grey,2,".",""); ?>€</span> <br />
+					<?php }?>
 					<?php if ($proyecto['precio_desmontaje'] > 0) { ?>
 						Precio desmontaje: <span><?php echo number_format($proyecto['precio_desmontaje'], 2, ".", ""); ?>€</span><br />
 					<?php } else {
